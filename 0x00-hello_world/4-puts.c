d8cb833b73e5e347f15eaefd9e7e34bea9f5bb49
markpkasi <pkasimark6@gmail.com>
2022-12-10 23:46:33 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - main block
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("\*programmimg is like building a multilingual puzzle");
-	return (0)
+	puts("\"programmimg is like building a multilingual puzzle");
+	return (0);
 }
