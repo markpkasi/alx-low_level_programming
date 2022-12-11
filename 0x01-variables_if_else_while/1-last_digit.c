1399babdc6a6eace8bf50a454926a24614637a36
markpkasi <pkasimark6@gmail.com>
2022-12-11 02:29:57 -0800

    Task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - points the last digit of the random
+ * main - prints the last digit of the random
  * number stored in the variable n
  * Return: Always 0 (Success)
  */
