677700aa3c3aa0d5c6ca89e08cc810630fff6d2f
markpkasi <pkasimark6@gmail.com>
2022-12-14 04:28:03 -0800

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,20 @@
 
 /**
  * main - Entry point
- *
- * Return: nothing, return void
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[9] = "_putchar";
 
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
