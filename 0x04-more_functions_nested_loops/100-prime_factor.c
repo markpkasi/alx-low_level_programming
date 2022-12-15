64bdec409add4205f5e0ab169ebbc6e185786eec
markpkasi <pkasimark6@gmail.com>
2022-12-15 14:00:22 -0800

    Advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,15 +18,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (i == 1)
 		{
@@ -34,10 +34,10 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
