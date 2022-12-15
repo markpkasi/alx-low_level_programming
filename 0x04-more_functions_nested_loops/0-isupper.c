ce76268ebfb4edeba6db6900df41edcf61d1665f
markpkasi <pkasimark6@gmail.com>
2022-12-15 10:22:47 -0800

    Task-0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 if parameter is uppercase
+ * @c: the parameter to check
+ * Return: 0 (Not success)
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
