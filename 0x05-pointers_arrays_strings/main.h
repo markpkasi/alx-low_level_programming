e6b9c2ed6986b9259afb82fd9362583139f751cd
markpkasi <pkasimark6@gmail.com>
2022-12-19 02:59:10 -0800

    Advanced tasks

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +17,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _putchar(char c);
 
+#endif /* MAIN_H_INCLUDED */
