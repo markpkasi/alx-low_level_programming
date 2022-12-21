e5d6afa29272e17d7e467b00b739b519b4cfddf4
markpkasi <pkasimark6@gmail.com>
2022-12-21 10:04:42 -0800

    a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		
+
 		'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 
 		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
@@ -28,9 +28,8 @@ char *rot13(char *s)
 
 		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
 
-		'L', 'M',
+		'L', 'M', 'N'};
 
-		           'N'};
 	for (stringCount = 0;
 			s[stringCount] != '\0';
 			stringCount++)
