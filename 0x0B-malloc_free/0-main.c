f5787f9051352c895207f105b951f3ada9d4fb75
markpkasi <pkasimark6@gmail.com>
2023-01-10 07:01:56 -0800

    malloc array

---
+++ 0x0B-malloc_free/0-main.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: address of memory to print
+ * @size: size of memory to print
+ *
+ * Return: nothing
+ */
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check code for students
+ *
+ * Return: 0
+ */
+int main void(void)
+{
+	char *buffer;
+
+	buffer = create_array(98, 'H');
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, 98);
+	free(buffer);
+	return (0);
+}
