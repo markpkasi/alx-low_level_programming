f34823f9336c6dfdc5dda34585bdb2eb52ea53ea
markpkasi <pkasimark6@gmail.com>
2023-01-11 06:21:08 -0800

    5-main.c

---
+++ 0x0B-malloc_free/5-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code for students
+ *
+ * Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
+}
