20af6839464f65f3ccf663a34dc195875929d046
markpkasi <pkasimark6@gmail.com>
2023-01-25 10:43:11 -0800

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,41 +2,44 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (success)
+ * print_opcodes - prints opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ * Return: void
  */
-int main(int argc, char *argv[])
-{
-	int bytes, i;
-	char *arr;
 
+void print_opcodes(char *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%.2hhx", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int n;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx", arr[i]);
-	}
+	print_opcodes((char *)&main, n);
 	return (0);
 }
